5867f17a5fdbedd70efe24c4d2e5e4e447a566b8
Huntmoon <wangtao576@foxmail.com>
2018-01-02 00:21:52 +0800

    增加注册中心使用

---
+++ bootstrap-context/src/main/java/com/huntmoon/bootstrap/context/Application.java
@@ -0,0 +1,17 @@
+package com.huntmoon.bootstrap.context;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @auther huntmoon
+ * @since 2017/12/30.
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
