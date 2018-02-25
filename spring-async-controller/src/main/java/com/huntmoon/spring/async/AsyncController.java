3d0f30631f8d6c3edf1d6e217a8b9fa3e1d5692f
wangtao <tao.wang@anxincloud.com>
2018-02-25 12:01:50 +0800

    异步控制器

--- spring-async-controller/src/main/java/com/huntmoon/spring/async/AsyncController.java
+++ spring-async-controller/src/main/java/com/huntmoon/spring/async/AsyncController.java
@@ -31,7 +31,7 @@ public class AsyncController {
         webDataBinder.registerCustomEditor(Person.class, new PropertyEditorSupport() {
             @Override
             public void setAsText(String text) throws IllegalArgumentException {
-                setValue(JSon);
+                setValue("fj");
             }
         });
     }
